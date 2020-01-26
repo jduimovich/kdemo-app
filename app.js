a31ef209e9221cf8bd150360519b57b977aa020e
John Duimovich <jduimovich@gmail.com>
2020-01-26 11:33:09 -0500

    sim mode for local dev

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ var os = require('os');
 
 var APP_VERSION = '2.0';
 var APP_COLOUR = 'yellow';
-
 var HOSTNAME = os.hostname();
+
+var SIM= process.env.SIM;
+if (SIM) console.log ("Running Simulated Data");
+var sim = { 
+	"balance": 50,
+	"v1": 1.0,
+	"v2": 2.0,
+	"c1": "blue",
+	"c2": "green"
+};
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+  }
 
 var count = 0;
 function addResponse(response, fromRequest) {
 	response.route = fromRequest.url;
 	response.time = new Date();
 	response.count = count++;
-	response.version = APP_VERSION;
 	response.hostname = HOSTNAME;
-	response.colour = APP_COLOUR;
+	if (SIM) { 
+		if (getRandomInt (100) < sim.balance)  { 
+			response.version = sim.v1;
+			response.colour = sim.c1; 
+		}  else { 
+			response.version = sim.v2;
+			response.colour = sim.c2; 
+		}
+	} else { 
+		response.version = APP_VERSION;
+		response.colour = APP_COLOUR;
+	}
 };
 
 app.get("/", function (req, res) {
@@ -51,6 +74,39 @@ app.get("/terminate-v1", function (req, res) {
 	res.send(JSON.stringify(response, undefined, 4));
 	process.exit (0);
 });
+ 
+if (SIM) { 
+app.get("/simconfig", function (req, res) { 
+	var left = req.query.balance;
+	if (left) {
+		console.log(req.url + " param = " + left);
+		sim.balance = left;
+	}
+	res.send( {
+		"left": sim.balance,
+		"right": 100-sim.balance,
+		"updateInProgress": false,
+		"proxy": true,
+		"timeForLastUpdate": 0,
+		"avgTimeForUpdate": 0
+	});
+});
+
+app.get("/colour", function (req, res) { 
+	var newColour = req.query.colour;
+	if (newColour) {
+		console.log(req.url + " colour = " + newColour);
+		sim.v1 = sim.v2;
+		sim.c1 = sim.c2;
+		sim.v2 = (sim.v2+1);
+		sim.c2 = newColour;
+	}
+	res.send( sim);
+});
+
+}
+
+
 
 
 const port = process.env.PORT || 8080;
